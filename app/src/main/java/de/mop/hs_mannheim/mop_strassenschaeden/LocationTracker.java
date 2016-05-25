c35cffeef83bdb20c8b33f4067052e0ce30179f9
sebbi08 <HSma1223522>
2016-05-25 16:01:14 +0200

    Major Release 25.5 set start Location

--- app/src/main/java/de/mop/hs_mannheim/mop_strassenschaeden/LocationTracker.java
+++ app/src/main/java/de/mop/hs_mannheim/mop_strassenschaeden/LocationTracker.java
@@ -17,56 +17,26 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
 
 
-public class LocationTracker implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class LocationTracker implements GoogleApiClient.OnConnectionFailedListener {
 
-    private Context activity;
+    private Activity activity;
 
     private GoogleApiClient mGoogleApiClient;
 
-    public LocationTracker(Context activity) {
+    public LocationTracker(Activity activity) {
         this.activity = activity;
         buildGoogleApiClient();
     }
 
     private synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(activity)
-                .addConnectionCallbacks(this)
+                .addConnectionCallbacks((GoogleApiClient.ConnectionCallbacks) activity)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
-    }
-
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-
-        LocationRequest mLocationRequest = LocationRequest.create();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(1000); // Update location every second
-
-        if (ContextCompat.checkSelfPermission(activity,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED && activity instanceof LocationListener) {
-
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, (LocationListener) activity);
-
-            Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                    mGoogleApiClient);
-
-            if(mLastLocation != null){
-                ((LocationListener) activity).onLocationChanged(mLastLocation);
-            }
-
-        }
-
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.i(this.getClass().getName(),"Connection Suspended with i : " + i);
     }
 
     @Override
@@ -80,5 +50,9 @@ public class LocationTracker implements GoogleApiClient.ConnectionCallbacks, Goo
 
     public void disconnect() {
         mGoogleApiClient.disconnect();
+    }
+
+    public GoogleApiClient getmGoogleApiClient() {
+        return mGoogleApiClient;
     }
 }
--- app/src/main/java/de/mop/hs_mannheim/mop_strassenschaeden/MainActivity.java
+++ app/src/main/java/de/mop/hs_mannheim/mop_strassenschaeden/MainActivity.java
@@ -1,14 +1,19 @@
 package de.mop.hs_mannheim.mop_strassenschaeden;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,48 +21,26 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback,LocationListener {
+import java.util.Objects;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks{
 
 
     private GoogleMap mMap;
     private LocationTracker tracker;
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(this);
 
-        // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 684);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        }
-
         tracker = new LocationTracker(this);
-        tracker.connect();
-
     }
 
 
@@ -73,15 +56,54 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        tracker.connect();
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        googleApiClient = tracker.getmGoogleApiClient();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},555);
+            return;
+        }
+
+        setLocation();
+
+    }
+
+    public void setLocation(){
+        Location loc = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        LatLng position = new LatLng(loc.getLatitude(),loc.getLongitude());
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(position,19.0f));
+        mMap.addMarker(new MarkerOptions().title("YOUR POSITION").position(position));
     }
 
     @Override
-    public void onLocationChanged(Location location) {
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(),location.getLongitude())));
+    public void onConnectionSuspended(int i) {
+        //TODO
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 555){
+            for(int i = 0;i<permissions.length;i++){
+                String permision = permissions[i];
+                int result = grantResults[i];
+                if(Objects.equals(permision, Manifest.permission.ACCESS_FINE_LOCATION) && result == PackageManager.PERMISSION_GRANTED){
+                    setLocation();
+                }
+            }
+        }
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        tracker.disconnect();
     }
 }
